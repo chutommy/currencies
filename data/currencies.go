515a6908a594a442ee25e91cf3a3b33b1b15d96e
Tommy Chu <tommychu2256@gmail.com>
2020-07-20 11:46:42 +0200

    add update

--- data/currencies.go
+++ data/currencies.go
@@ -1,13 +1,37 @@
 package data
 
+import (
+	"fmt"
+
+	"github.com/chutified/currencies/models"
+)
+
+// DataService defines the data fetched from the website source.
 type DataService struct {
-	currencies map[string]float64
+	currencies map[string]*models.Currency
 }
 
+// New constructs a new data service.
 func New() *DataService {
-	ds := &DataService{
-		currencies: make(map[string]float64),
+	return &DataService{
+		currencies: make(map[string]*models.Currency),
+	}
+}
+
+// Update updates the currencies data.
+func (ds *DataService) Update() error {
+
+	// get data
+	rs, err := fetchData()
+	if err != nil {
+		return fmt.Errorf("fetching data: %w", err)
+	}
+	m, err := parseRecords(rs)
+	if err != nil {
+		return fmt.Errorf("parsing records: %w", err)
 	}
 
-	return ds
+	// success
+	ds.currencies = m
+	return nil
 }
