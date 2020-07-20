578c592b3651066b2cf7c89be80e675145a3e773
Tommy Chu <tommychu2256@gmail.com>
2020-07-20 10:08:35 +0200

    lower rate but size

--- data/fetching.go
+++ data/fetching.go
@@ -2,9 +2,11 @@ package data
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/chutified/currencies/models"
 )
 
 // record is a cache structure which holds raw source data.
@@ -26,12 +28,13 @@ func fetchData() ([]record, error) {
 		return nil, fmt.Errorf("fetch source website: %w", err)
 	}
 
-	// prepare saving memory
+	// prepare cache memory
 	var records []record
 	// loop over rows
 	doc.Find(".row-hover").Each(func(i int, s *goquery.Selection) {
 		children := s.Children()
 
+		// select
 		name := children.First()
 		ctr := name.Next()
 		dsc := ctr.Next().Next()
@@ -39,10 +42,12 @@ func fetchData() ([]record, error) {
 		prc := chP.Next()
 		last := prc.Next()
 
+		// filter
 		name = name.Children().First()
 		dsc = dsc.Children().First()
 		chP = chP.Children().First()
 
+		// trim spaces
 		n := strings.TrimSpace(name.Text())
 		c := strings.TrimSpace(ctr.Text())
 		d := strings.TrimSpace(dsc.Text())
@@ -63,4 +68,30 @@ func fetchData() ([]record, error) {
 	})
 
 	return records, nil
+}
+
+func parseRecords(rs []record) (map[string]models.Currency, error) {
+
+	// prepare cache memory
+	var cs map[string]models.Currency
+	for _, r := range rs {
+
+		// name
+		var name string
+		_, err := fmt.Sscanf(r.name, "USD/%s", &name)
+		if err != nil {
+			return nil, fmt.Errorf("unexpected name format %s", err)
+		}
+
+		// country
+		country := r.country
+
+		// description
+		description := r.description
+
+		// changeP
+		strconv.ParseFloat(r.changeP, 32)
+	}
+
+	return cs, nil
 }
