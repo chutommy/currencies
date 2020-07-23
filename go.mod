239d6b69abce2e60a3cf95deaa01b31c037ca59d
Tommy Chu <tommychu2256@gmail.com>
2020-07-23 19:41:24 +0200

    add subscription handle

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
-	github.com/chutified/resource-finder v0.0.0-20200718062720-b1337608abe3 // indirect
+	github.com/chutified/resource-finder v0.0.0-20200718062720-b1337608abe3
 	github.com/golang/protobuf v1.4.2
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.30.0
--- server/currencies.go
+++ server/currencies.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"time"
 
@@ -13,6 +15,9 @@ import (
 type Currency struct {
 	log *log.Logger
 	ds  *data.Service
+
+	// subscriptions holds all subscriptions for each client
+	subscriptions map[currency.Currency_SubscribeCurrencyServer][]*currency.GetCurrencyRequest
 }
 
 // New is a constructor of the currency server.
@@ -20,6 +25,8 @@ func New(log *log.Logger, ds *data.Service) *Currency {
 	c := &Currency{
 		log: log,
 		ds:  ds,
+
+		subscriptions: make(map[currency.Currency_SubscribeCurrencyServer][]*currency.GetCurrencyRequest),
 	}
 
 	// monitoring
@@ -58,6 +65,53 @@ func (c *Currency) GetRate(ctx context.Context, req *currency.GetRateRequest) (*
 
 // SubscribeCurrency handles SubscribeCurrency gRPC calls.
 func (c *Currency) SubscribeCurrency(srv currency.Currency_SubscribeCurrencyServer) error {
+	for {
+
+		// get request
+		req, err := srv.Recv()
+		if err == io.EOF {
+			c.log.Printf("[exit] client closed SubscribeCurrency call")
+
+			// TODO handle error the grpc way
+			break
+		}
+		if err != nil {
+			c.log.Printf("[error] invalid request format: %v", err)
+
+			// TODO handle error the grpc way
+			continue
+		}
+		name := req.GetName()
+
+		// validate request
+		if _, ok := c.ds.Currencies[name]; !ok {
+			c.log.Printf("[error] currency %v not found", name)
+
+			// TODO handle error the grpc way
+			continue
+		}
+
+		// check duplicates
+		var validErr error
+		for _, r := range c.subscriptions[srv] {
+			if r.GetName() == name {
+				validErr = fmt.Errorf("the client has already subscribed to %s", r.GetName())
+
+				// TODO handle error the grpc way
+				break
+			}
+		}
+		if validErr != nil {
+			c.log.Printf("[error] validation error: %v", err)
+
+			// TODO handle error the grpc way
+			continue
+		}
+
+		// append
+		c.subscriptions[srv] = append(c.subscriptions[srv], req)
+	}
+
 	return nil
 }
 
