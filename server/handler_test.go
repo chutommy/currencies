23f35a8db8defc1ca75d8732ab09ee0ac28f289e
Tommy Chu <tommychu2256@gmail.com>
2020-07-24 16:23:01 +0200

    add update handler test

--- server/handler_test.go
+++ server/handler_test.go
@@ -1,5 +1,54 @@
 package server
 
-import "testing"
+import (
+	"bytes"
+	"log"
+	"strings"
+	"testing"
+	"time"
 
-func TestHandleUpdates(t *testing.T) {}
+	data "github.com/chutified/currencies/data"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestHandleUpdates(t *testing.T) {
+	ss := bytes.NewBufferString("")
+	log := log.New(ss, "", log.LstdFlags)
+	ds := data.New()
+	err := ds.Update("https://markets.businessinsider.com/currencies")
+	if err != nil {
+		t.Fatal("could not update data")
+	}
+	c := &Currency{
+		log: log,
+		ds:  ds,
+	}
+
+	*ss = *(bytes.NewBufferString(""))
+	t.Run("ok", func(t1 *testing.T) {
+		// TODO add subscribtions
+
+		go func() {
+			c.handleUpdates(500*time.Millisecond, "https://markets.businessinsider.com/currencies")
+		}()
+		time.Sleep(1 * time.Second)
+
+		assert.Equal(t1, strings.Contains(ss.String(), "[update]"), true)
+	})
+
+	*ss = *(bytes.NewBufferString(""))
+	t.Run("invalid url", func(t1 *testing.T) {
+		c := &Currency{
+			log: log,
+			ds:  ds,
+		}
+
+		go func() {
+			c.handleUpdates(1*time.Millisecond, "invalid url")
+		}()
+		time.Sleep(1 * time.Second)
+
+		assert.Equal(t1, strings.Contains(ss.String(), "[error]"), true)
+	})
+
+}
