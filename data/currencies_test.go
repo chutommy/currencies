f2f1564d1a92e822dec696616cd213eafd90621a
Tommy Chu <tommychu2256@gmail.com>
2020-07-24 10:24:34 +0200

    add service monitor data test

--- data/currencies_test.go
+++ data/currencies_test.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"testing"
+	"time"
 
 	"gopkg.in/go-playground/assert.v1"
 )
@@ -25,5 +26,40 @@ func TestUpdate(t *testing.T) {
 	t.Run("invalid source content", func(t1 *testing.T) {
 		err := s.Update("https://markets.businessinsider.com/commodities")
 		assert.MatchRegex(t1, err.Error(), ".*parsing records.*")
+	})
+}
+
+func TestMonitorData(t *testing.T) {
+	s := New()
+
+	t.Run("ok", func(t1 *testing.T) {
+		upds, errs := s.MonitorData(500*time.Millisecond, "https://markets.businessinsider.com/currencies")
+
+		var err error
+		go func() {
+			err = <-errs
+		}()
+		go func() {
+			_ = <-upds
+		}()
+		time.Sleep(1 * time.Second)
+
+		assert.Equal(t1, err, nil)
+
+	})
+
+	t.Run("invalid URL", func(t1 *testing.T) {
+		upds, errs := s.MonitorData(500*time.Millisecond, "invalid_URL")
+
+		var err error
+		go func() {
+			err = <-errs
+		}()
+		go func() {
+			_ = <-upds
+		}()
+		time.Sleep(1 * time.Second)
+
+		assert.NotEqual(t1, err, nil)
 	})
 }
